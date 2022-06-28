d350e86d0ce8a78254090cb831f7020017e2cf40
Gabriel Cammany <gabriel.cammany@pm.me>
2022-06-28 17:55:11 +0200

    feat: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    setupFilesAfterEnv: ["jest-extended/all"],
+    // TODO: Disable temporally metadata tests due to requiring postgres
+    modulePathIgnorePatterns: [
+        "/dist/",
+        "/types/",
+    ],
+    // TODO: Remove this when tests are separated to unit and integration tests
+    maxConcurrency: 1,
+    maxWorkers: 1,
+    testTimeout: 50000,
+    collectCoverage: true,
+    coveragePathIgnorePatterns: ["/dist/", "/__mocks__/"],
+    verbose: true,
+    testRegex: '/.*\.test\.ts$'
+};
+export default config;
