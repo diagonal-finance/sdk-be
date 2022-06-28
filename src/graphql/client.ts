05a0e8d2083a5d2f051bae4e47115a346d0b2ba6
Gabriel Cammany <gabriel.cammany@pm.me>
2022-06-28 17:54:38 +0200

    feat: add graphql client and schema

---
+++ src/graphql/client.ts
@@ -0,0 +1,63 @@
+import {
+    ClientError,
+    GraphQLClient as GraphQLClientRequest,
+} from "graphql-request";
+
+import {
+    InternalServiceError,
+    InvalidInputError,
+    UnauthorizedError,
+} from "../error";
+
+import { getSdk, SdkFunctionWrapper } from "./generated";
+
+export type GraphQLClient = ReturnType<typeof getSdk>;
+
+const apiUrl = "http://localhost:4000/graphql";
+
+function handleClientError(error: ClientError) {
+    const response = error.response;
+    if (response.errors && response.errors.length > 0) {
+        const isAuthError = response.errors.find(
+            (responseError) => responseError.extensions.code === "UNAUTHORIZED"
+        );
+        if (isAuthError) {
+            throw new UnauthorizedError("Authorization error");
+        }
+        const isBadInputError = response.errors.find(
+            (responseError) =>
+                responseError.extensions.code === "BAD_USER_INPUT"
+        );
+        if (isBadInputError) {
+            throw new InvalidInputError("Invalid input value provided");
+        }
+    }
+}
+
+const wrapper: SdkFunctionWrapper = async (action) => {
+    try {
+        return await action();
+    } catch (error) {
+        console.log(error);
+        if (error instanceof ClientError) {
+            handleClientError(error);
+        }
+    }
+    throw new InternalServiceError(
+        "Unhandled error while performing the API request"
+    );
+};
+
+export const getGraphQLClient = (
+    apiKey: string,
+    url?: string
+): GraphQLClient => {
+    return getSdk(
+        new GraphQLClientRequest(url ?? apiUrl, {
+            headers: {
+                "x-api-key": apiKey,
+            },
+        }),
+        wrapper
+    );
+};
---
+++ src/graphql/operations/mutations.ts
@@ -0,0 +1,12 @@
+import { gql } from "graphql-request";
+
+export const CREATE_CHECKOUT_SESSION_MUTATION = gql`
+    mutation CheckoutSessionCreate($input: CheckoutSessionCreateInput!) {
+        checkoutSessionCreate(input: $input) {
+          ... on CheckoutSession {
+            id
+            url
+          }
+        }
+    }
+`;
