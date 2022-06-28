f3c021eba67e865552541741ad2e28c5b79d883e
Gabriel Cammany <gabriel.cammany@pm.me>
2022-06-28 17:56:33 +0200

    feat: added chains and token config

---
+++ src/config/chains.ts
@@ -0,0 +1,7 @@
+export enum ChainId {
+    Rinkeby = 4,
+    Kovan = 42,
+    Polygon = 137,
+    Mumbai = 80001,
+    ArbitrumRinkeby = 421611,
+}
---
+++ src/config/tokens.ts
@@ -0,0 +1,37 @@
+export enum Token {
+  DAI = 'DAI',
+  USDC = 'USDC',
+
+  fDAI = 'fDAI',
+  fUSDC = 'fUSDC',
+
+  USDCx = 'USDCx',
+  MATICx = 'MATICx',
+  ETHx = 'ETHx',
+  DAIx = 'DAIx',
+
+  fDAIx = 'fDAIx',
+  fUSDCx = 'fUSDCx',
+}
+
+export const enum RealToken {
+  DAI = 'DAI',
+  USDC = 'USDC',
+}
+
+export const enum FakeToken {
+  fDAI = 'fDAI',
+  fUSDC = 'fUSDC',
+}
+
+export const enum SuperToken {
+  USDCx = 'USDCx',
+  MATICx = 'MATICx',
+  ETHx = 'ETHx',
+  DAIx = 'DAIx',
+}
+
+export const enum FakeSuperToken {
+  fDAIx = 'fDAIx',
+  fUSDCx = 'fUSDCx',
+}
