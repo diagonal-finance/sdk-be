31a4fca4cda7de1a6369f8d971c6fd6a4257dc07
Gabriel Cammany <gabriel.cammany@pm.me>
2022-07-15 12:14:45 +0200

    feat: add optimism and arbitrum

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,7 +1,13 @@
 export enum ChainId {
     Rinkeby = 4,
     Kovan = 42,
+
     Polygon = 137,
     Mumbai = 80001,
+
+    Optimism = 10,
+    OptimismKovan = 69,
+
+    Arbitrum = 42161,
     ArbitrumRinkeby = 421611,
 }
